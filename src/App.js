9022866e567807fd9c82993d05807735ee8916d5
dpajek <dan.pajek@gmail.com>
2020-09-23 22:09:10 -0400

    noopener noreferrer

--- src/App.js
+++ src/App.js
@@ -14,17 +14,33 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            <a
+              class="navbar-brand"
+              href="https://codingthesmartway.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
             </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
                 </li>
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
                 </li>
               </ul>
             </div>
